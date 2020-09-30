c6838b3714db7d938461172d3b23f4b876656ecd
cleminator <alvinclemens@gmail.com>
2020-09-29 21:22:58 -0600

    weeee

--- server.js
+++ server.js
@@ -6,7 +6,9 @@ var path = require("path");
 // Sets up the Express App
 // =============================================================
 var app = express();
-var PORT = 3000;
+var PORT = process.env.PORT || 3000;
+// this will run in herokue 
+// 3000 is for our local macine 
 
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
